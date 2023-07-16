151becaefbbec809b8619ef204711a3586033d2c
TonyoGodson <59567558+TonyoGodson@users.noreply.github.com>
2023-07-16 18:05:00 +0100

    first reusable progressbar example

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:testing_reusable_widgets/custom_progressbar.dart';
+import 'package:testing_reusable_widgets/widgets_screen.dart';
 
 
 void main(){
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: const Text("Reusable Widgets"),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          children: [
-            customProgressBar(0, true, 750, 20, Colors.red, Colors.red, 60, 10, 0)
-          ],
-        ),
-      ),
-    ),
+  runApp(const MaterialApp(
+    home: WidgetsScreen(),
   ));
 }
---
+++ lib/widgets_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'custom_progressbar.dart';
+
+class WidgetsScreen extends StatefulWidget {
+  const WidgetsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetsScreen> createState() => _WidgetsScreenState();
+}
+
+class _WidgetsScreenState extends State<WidgetsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Reusable Widgets"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const Text("A Red Reusable Progress Bar"),
+            const SizedBox(height: 5),
+            customProgressBar(0.0, true, 750, 20.0, Colors.red,
+                const Color(0x33FF0000), .2, 10.0, 0.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
